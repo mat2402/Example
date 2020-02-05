7b6c95e874a509dd6f7248d010a0142a838da708
Mateusz <mateuszmjwsk@gmail.com>
2020-02-05 17:04:24 +0100

    Committing

--- src/AnagramChecker.java
+++ src/AnagramChecker.java
@@ -6,6 +6,7 @@ public class AnagramChecker {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("   ");
+		System.out.println("     ");
 
 		System.out.println("First word: ");
 		String firstWord = scanner.next();
