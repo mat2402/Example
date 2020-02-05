6c2929defa50923128663a6e1db9169f4f62293f
Mateusz <mateuszmjwsk@gmail.com>
2020-02-05 16:56:24 +0100

    Committing

--- src/AnagramChecker.java
+++ src/AnagramChecker.java
@@ -5,6 +5,7 @@ public class AnagramChecker {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		System.out.println("   ");
 
 		System.out.println("First word: ");
 		String firstWord = scanner.next();
