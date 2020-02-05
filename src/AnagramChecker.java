f487f2aa7786cbc2182e2ea27c7a4f9ac3cf9758
Mateusz <mateuszmjwsk@gmail.com>
2020-02-05 17:22:47 +0100

    Committing

--- src/AnagramChecker.java
+++ src/AnagramChecker.java
@@ -9,6 +9,8 @@ public class AnagramChecker {
 		System.out.println("   ");
 		System.out.println(" ");
 		System.out.println(" ");
+		System.out.println(" ");
+		System.out.println(" ");
 
 		System.out.println("First word: ");
 		String firstWord = scanner.next();
